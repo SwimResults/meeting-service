4af1c326498c212abbeb76d52c465c9ea90caeab
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-07-31 16:45:33 +0200

    meeting data ftp info

--- model/meeting_data.go
+++ model/meeting_data.go
@@ -7,7 +7,8 @@ type MeetingData struct {
 	InstagramUrl      string `json:"instagram_url,omitempty" bson:"instagram_url,omitempty"`
 	FacebookUrl       string `json:"facebook_url,omitempty" bson:"facebook_url,omitempty"`
 	HasLivetiming     bool   `json:"has_livetiming,omitempty" bson:"has_livetiming,omitempty"`
-	HasFtpUpload      bool   `json:"has_ftp_upload,omitempty" bson:"has_ftp_upload,omitempty"`
+	HasFtpStartList   bool   `json:"has_ftp_start_list,omitempty" bson:"has_ftp_start_list,omitempty"`
+	HasFtpResultList  bool   `json:"has_ftp_result_list,omitempty" bson:"has_ftp_result_list,omitempty"`
 	FtpStartListMask  string `json:"ftp_start_list_mask,omitempty" bson:"ftp_start_list_mask,omitempty"`
 	FtpResultListMask string `json:"ftp_result_list_mask,omitempty" bson:"ftp_result_list_mask,omitempty"`
 }
