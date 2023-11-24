4bc85d1ef56b967c3ceb80131f3ec4855860cd6d
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-11-24 15:09:27 +0100

    age group client; add age group import and get requests to age group client;

---
+++ client/age_group_client.go
@@ -0,0 +1,64 @@
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/swimresults/meeting-service/dto"
+	"github.com/swimresults/meeting-service/model"
+	"github.com/swimresults/service-core/client"
+	"net/http"
+	"strconv"
+)
+
+type AgeGroupClient struct {
+	apiUrl string
+}
+
+func NewAgeGroupClient(url string) *AgeGroupClient {
+	return &AgeGroupClient{apiUrl: url}
+}
+
+func (c *AgeGroupClient) ImportAgeGroup(ageGroup model.AgeGroup) (*model.AgeGroup, bool, error) {
+	request := dto.ImportAgeGroupRequestDto{
+		AgeGroup: ageGroup,
+	}
+
+	res, err := client.Post(c.apiUrl, "age_group/import", request)
+	if err != nil {
+		return nil, false, err
+	}
+	defer res.Body.Close()
+
+	newAgeGroup := &model.AgeGroup{}
+	err = json.NewDecoder(res.Body).Decode(newAgeGroup)
+	if err != nil {
+		return nil, false, err
+	}
+
+	if res.StatusCode != http.StatusCreated && res.StatusCode != http.StatusOK {
+		return nil, false, fmt.Errorf("import age group request returned: %d", res.StatusCode)
+	}
+	return newAgeGroup, res.StatusCode == http.StatusCreated, nil
+}
+
+func (c *AgeGroupClient) GetAgeGroupsForMeetingAndEvent(meeting string, number int) (*[]model.AgeGroup, error) {
+	fmt.Printf("request '%s'\n", c.apiUrl+"/age_group/meet/"+meeting+"/event/"+strconv.Itoa(number))
+
+	res, err := client.Get(c.apiUrl, "age_group/meet/"+meeting+"/event/"+strconv.Itoa(number), nil)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("GetAgeGroupsForMeetingAndEvent received error: %d\n", res.StatusCode)
+	}
+
+	ageGroups := &[]model.AgeGroup{}
+	err = json.NewDecoder(res.Body).Decode(ageGroups)
+	if err != nil {
+		return nil, err
+	}
+
+	return ageGroups, nil
+}
