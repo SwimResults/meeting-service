83407ab95ce1c6b9118141574d9bdbd8dcca7d5c
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2024-11-29 14:34:31 +0100

    add meeting client

---
+++ client/meeting_client.go
@@ -0,0 +1,39 @@
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/swimresults/meeting-service/model"
+	"github.com/swimresults/service-core/client"
+	"net/http"
+)
+
+type MeetingClient struct {
+	apiUrl string
+}
+
+func NewMeetingClient(url string) *MeetingClient {
+	return &MeetingClient{apiUrl: url}
+}
+
+func (c *MeetingClient) GetMeetingById(id string) (*model.Meeting, error) {
+	fmt.Printf("request '%s'\n", c.apiUrl+"meeting/"+id)
+
+	res, err := client.Get(c.apiUrl, "meeting/"+id, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("GetMeetingById received error: %d\n", res.StatusCode)
+	}
+
+	meeting := &model.Meeting{}
+	err = json.NewDecoder(res.Body).Decode(meeting)
+	if err != nil {
+		return nil, err
+	}
+
+	return meeting, nil
+}
